db8235650e924a47f3633abc67b24fb26ef9c630
HP <923112087@qq.com>
2021-01-13 19:57:10 +0800

    修改

---
+++ rent_common/src/main/java/com/heikes/rent_common/pojo/Users.java
@@ -0,0 +1,150 @@
+package com.heikes.rent_common.pojo;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 用户表
+ * </p>
+ *
+ * @author yanxin
+ * @since 2020-12-24
+ */
+public class Users implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+    /**
+     * 联系电话
+     */
+    private String phone;
+
+    /**
+     * 身份证号
+     */
+    private String idCard;
+
+    /**
+     * 0代表未被封禁，1代表被封禁
+     */
+    private Integer userStatus;
+
+    /**
+     *用户头像
+     */
+    private String userImage;
+
+    /**
+     * 评论权限，0代表正常，1代表被封禁
+     **/
+    private Integer commentStatus;
+
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getUserAccount() {
+        return userAccount;
+    }
+
+    public void setUserAccount(String userAccount) {
+        this.userAccount = userAccount;
+    }
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+    public Integer getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(Integer userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Integer getCommentStatus() {
+        return commentStatus;
+    }
+
+    public void setCommentStatus(Integer commentStatus) {
+        this.commentStatus = commentStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", userAccount='" + userAccount + '\'' +
+                ", phone='" + phone + '\'' +
+                ", idCard='" + idCard + '\'' +
+                ", userStatus=" + userStatus +
+                ", userImage='" + userImage + '\'' +
+                ", commentStatus=" + commentStatus +
+                ", role=" + role +
+                '}';
+    }
+}
--- rent_common/src/main/java/com/heikes/rent_common/remote/user/RoleRemote.java
+++ rent_common/src/main/java/com/heikes/rent_common/remote/user/RoleRemote.java
@@ -2,8 +2,10 @@ package com.heikes.rent_common.remote.user;
 
 import com.heikes.rent_common.dto.Bizdto;
 import com.heikes.rent_common.pojo.Role;
+import com.heikes.rent_common.vo.PageResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Map;
@@ -18,4 +20,8 @@ import java.util.Map;
 public interface RoleRemote {
     @PostMapping(value = "selectRoles")
     Bizdto<List<Role>> selectRoles(@RequestBody  Map<String, Object> map);
+
+    @PostMapping(value = "selectRoleByPage")
+    Bizdto<PageResult<Role>> selectRoleByPage(@RequestParam(value = "pageNum") Integer pageNum,
+                                              @RequestParam(value = "pageSzie") Integer pageSize);
 }
